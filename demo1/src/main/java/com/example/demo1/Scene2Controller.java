94498af758c97733dc5c9779884453c002a4bc81
amerkobari <s12029312@stu.najah.edu>
2023-05-09 17:58:49 +0300

    no message

--- demo1/src/main/java/com/example/demo1/Scene2Controller.java
+++ demo1/src/main/java/com/example/demo1/Scene2Controller.java
@@ -34,15 +34,6 @@ public class Scene2Controller {
     @FXML
     private TabPane tabPane;
 
-    @FXML
-    private Tab Maintab;
-
-    @FXML
-    private Tab Shippingtab;
-
-    @FXML
-    private Button NEXTbutton;
-
     String itemname ;
     String itemsize;
     String cleaningtype;
@@ -56,6 +47,8 @@ public class Scene2Controller {
     }
 
     private ActionEvent event;
+
+    Scene1Controller s = new Scene1Controller();
 
 
     public void switchScene1(ActionEvent event) throws IOException {
@@ -336,7 +329,7 @@ public boolean isclicked =false;
     private void Bwr(String filename) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
         CustomerId = String.format("%03d", counter);
-        writer.write(CustomerId + "\t" + Scene1Controller.username + "\t" + DelText.getText() + "\t" + itemname + "\t" + itemsize + "\t" + cleaningtype + "\t" + Price + "\n");
+        writer.write(CustomerId + "\t" + s.getUsername() + "\t" + DelText.getText() + "\t" + itemname + "\t" + itemsize + "\t" + cleaningtype + "\t" + Price + "\n");
         counter++;
         MsgText = "Your order with IDnumber " + CustomerId + " has been accepted and will be processed shortly, it will be sent to this location when it's done " + DelText.getText() + " Thank you for choosing us";
         writer.close();
@@ -346,7 +339,7 @@ public boolean isclicked =false;
     private void Bwrr(String filename) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
         CustomerId = String.format("%03d", counter);
-        writer.write(CustomerId + "\t" + Scene1Controller.username + "\t" + "" +  "\t" + itemname + "\t" + itemsize + "\t" + cleaningtype + "\t" + res + "\n");
+        writer.write(CustomerId + "\t" + s.getUsername() + "\t" + "" +  "\t" + itemname + "\t" + itemsize + "\t" + cleaningtype + "\t" + res + "\n");
         counter++;
         MsgText = "Your order with IDnumber " + CustomerId + " has been accepted and will be processed shortly, We will send you an email when it's ready to pickup, Thank you for choosing us";
         writer.close();
@@ -358,7 +351,7 @@ public boolean isclicked =false;
         Scene1Controller s1 = new Scene1Controller();
 
 
-       String email12 = s1.getEmailAddress(Scene1Controller.username);
+       String email12 = s1.getEmailAddress(s1.getUsername());
         String subject = OrderAccepted;
         String messageBody = MsgText;
         sendEmail x = new sendEmail(email12, subject, messageBody);
