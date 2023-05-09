a524c708e00184d79620f3449b788162296fbab4
Tariq_mrmsh <tariqmarmash17@gmail.com>
2023-05-09 17:59:44 +0300

    no message

--- demo1/src/main/java/com/example/demo1/Scene2Controller.java
+++ demo1/src/main/java/com/example/demo1/Scene2Controller.java
@@ -336,7 +336,7 @@ public boolean isclicked =false;
     private void Bwr(String filename) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
         CustomerId = String.format("%03d", counter);
-        writer.write(CustomerId + "\t" + Scene1Controller.username + "\t" + DelText.getText() + "\t" + itemname + "\t" + itemsize + "\t" + cleaningtype + "\t" + Price + "\n");
+        writer.write(CustomerId + "\t" +  + "\t" + DelText.getText() + "\t" + itemname + "\t" + itemsize + "\t" + cleaningtype + "\t" + Price + "\n");
         counter++;
         MsgText = "Your order with IDnumber " + CustomerId + " has been accepted and will be processed shortly, it will be sent to this location when it's done " + DelText.getText() + " Thank you for choosing us";
         writer.close();
